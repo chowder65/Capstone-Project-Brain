33fa337f5ebcd0db87f8d5fb6c899cde59913fb7
Christian McCulley <127411172+chowder65@users.noreply.github.com>
2025-01-14 18:13:51 -0700

    worked on the ui and now have a functioning ui just need to add the api reference in

--- vue-desktop/src/components/HelloWorld.vue
+++ vue-desktop/src/components/HelloWorld.vue
@@ -1,58 +1,332 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+    <div class="loginBox">
+      <div class="inner">
+        <div class="signIn" v-if="signIn">
+          <div class="top">
+            <img
+              class="logo"
+              src="https://res.cloudinary.com/dc3c8nrut/image/upload/v1685298768/logo-placeholder_l3yodl.png"
+            />
+            <div class="title">Sign in</div>
+            <div class="subtitle">
+              Don't have an account?
+              <span class="subtitle-action" @click="signIn = !signIn">
+                Create Account
+              </span>
+            </div>
+          </div>
+          <form>
+            <div class="form">
+              <input
+                required
+                aria-required="true"
+                aria-invalid="false"
+                aria-label="E-mail"
+                type="email"
+                pattern="^[\w\.-]+@[\w\.-]+\.\w+$"
+                class="w100"
+                :class="{ invalid: email.error }"
+                ref="email"
+                placeholder="Email"
+                autofocus
+                @blur="validateEmail"
+                @keydown="validateEmail"
+                v-model="email.value"
+              />
+
+              <input
+                required
+                aria-required="true"
+                type="password"
+                class="w100"
+                :class="{ invalid: password.error }"
+                placeholder="Password"
+                v-model="password.value"
+                @blur="validatePassword"
+                @keydown="validatePassword"
+              />
+            </div>
+
+            <input
+              type="submit"
+              value="Submit"
+              class="action"
+              :class="{ 'action-disabled': !loginValid }"
+            />
+          </form>
+        </div>
+
+        <div class="register" v-else>
+          <div class="top">
+            <img
+              class="logo"
+              src="https://res.cloudinary.com/dc3c8nrut/image/upload/v1685298768/logo-placeholder_l3yodl.png"
+            />
+            <div class="title">Create an Account</div>
+            <div class="subtitle">
+              Already have an account?
+              <span class="subtitle-action" @click="signIn = !signIn">
+                Sign In
+              </span>
+            </div>
+          </div>
+
+          <div class="form">
+            <input
+              type="text"
+              placeholder="First name"
+              autofocus
+              v-model="firstName"
+              class="w100"
+            />
+
+            <input
+              type="text"
+              placeholder="Last name"
+              v-model="lastName"
+              class="w100"
+            />
+
+            <input
+              type="text"
+              class="w100"
+              placeholder="Email"
+              v-model="email.value"
+            />
+            <input
+              type="password"
+              class="w100"
+              placeholder="Password"
+              v-model="password.value"
+            />
+          </div>
+
+          <button class="action" :class="{ 'action-disabled': !registerValid }">
+            Create Account
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      emailRegex: /^[\w.-]+@[\w.-]+\.\w+$/,
+      passwordRegex: /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/,
+
+      firstName: "",
+      lastName: "",
+
+      password: {
+        value: "",
+        error: false
+      },
+
+      email: {
+        value: "",
+        error: false
+      },
+
+      signIn: true
+    };
+  },
+
+  methods: {
+    validateEmail() {
+  this.email.error = this.email.value === "";
+},
+
+validatePassword() {
+  this.password.error = this.password.value === "";
+},
+  },
+
+  mounted() {
+    // this.$refs.email.focus();
+  },
+
+  computed: {
+    validFirstName() {
+      return this.firstName.length > 0;
+    },
+
+    validLastName() {
+      return this.lastName.length > 0;
+    },
+
+    emailValid() {
+      return this.emailRegex.test(this.email.value);
+    },
+
+    passwordValid() {
+      return this.password.value.length > 0;
+    },
+
+    loginValid() {
+      return this.emailValid && this.passwordValid;
+    },
+
+    registerValid() {
+      return (
+        this.emailValid &&
+        this.passwordValid &&
+        this.validFirstName &&
+        this.validLastName
+      );
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<!-- Use preprocessors via the lang attribute! e.g. <style lang="scss"> -->
+<style lang="scss">
+@mixin box {
+  box-shadow: 1px 1px 2px 1px #ccc;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+@mixin field {
+  border: 1px solid #aaa;
+  height: 40px;
+  padding: 10px;
+  margin-top: 20px;
+  border-radius: 5px;
+  box-sizing: border-box;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+input[type="text"] {
+  @include field;
 }
-a {
-  color: #42b983;
+
+input[type="email"] {
+  @include field;
+}
+
+input[type="password"] {
+  @include field;
+}
+
+.invalid {
+  border: 2px solid red !important;
+  &::placeholder {
+    color: red;
+  }
+}
+
+.errorMessage {
+  color: red;
+  margin: 10px;
+  top: 5px;
+}
+
+.w100 {
+  width: 100%;
+}
+
+#app {
+}
+
+.logo {
+  width: 300px;
+  margin-bottom: 10px;
+}
+
+.action {
+  height: 40px;
+  text-transform: uppercase;
+  border-radius: 25px;
+  width: 100%;
+  border: none;
+  cursor: pointer;
+  background: green;
+  margin-top: 20px;
+  color: #fff;
+  font-size: 1.2rem;
+  @include box;
+}
+
+.action-disabled {
+  color: #eee;
+  background: #aaa;
+  cursor: auto;
+}
+
+.top {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: 10px;
+}
+
+.title {
+  width: 100%;
+  font-size: 1.8rem;
+  margin-bottom: 10px;
+  text-align: center;
+}
+
+.subtitle {
+  .subtitle-action {
+    color: green;
+    font-weight: bold;
+    cursor: pointer;
+  }
+}
+
+html {
+  background-repeat: no-repeat;
+  background: linear-gradient(
+    to bottom,
+    rgba(96, 108, 136, 1) 0%,
+    rgba(63, 76, 107, 1) 100%
+  );
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-family: sans-serif;
+}
+
+.loginBox {
+  background: #fff;
+  border-radius: 15px;
+  max-width: 400px;
+  padding: 25px 55px;
+  animation: slideInTop 1s;
+}
+
+@keyframes slideInTop {
+  from {
+    opacity: 0;
+    transform: translateY(-30%);
+  }
+
+  to {
+    opacity: 100;
+    transform: translateY(0%);
+  }
+}
+
+@media screen and (min-width: 440px) {
+  .loginBox {
+    @include box;
+  }
+}
+
+@media screen and (max-width: 440px) {
+  html {
+    background: #fff;
+    align-items: start;
+    justify-content: start;
+  }
+
+  .loginBox {
+    padding: 25px 25px;
+    max-width: 100vw;
+  }
 }
 </style>
